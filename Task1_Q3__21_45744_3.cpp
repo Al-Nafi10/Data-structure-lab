dd7a5cba1ff53f67c454df05f85a022e2c48ba20
Al-Nafi10 <114634375+Al-Nafi10@users.noreply.github.com>
2022-09-30 01:19:37 +0600

    Add files via upload

---
+++ Task1_Q3__21_45744_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int firstVal, lastVal;
+
+    cout << "Please enter the starting value of the range : ";
+    cin >> firstVal;
+    cout << "Please enter the last value of the range : ";
+    cin >> lastVal;
+
+    int size = (lastVal - firstVal) + 1;
+
+    int arr[size];
+
+    for(int i = 0; i < size; i++){
+        arr[i] = firstVal;
+        firstVal++;
+    }
+
+    cout << "All the odd numbers in the given range : ";
+    for(int i = 0; i < size; i++){
+       if(arr[i]%2!=0){
+            cout << arr[i] << ", ";
+        }
+    }
+}
